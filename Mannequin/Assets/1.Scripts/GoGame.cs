9868fe37a78723bbfcddbc7626efd40594929e76
shingugitvr000 <shingugitvr000@gmail.com>
2023-06-22 20:43:01 +0900

    커서 락 풀기

--- Mannequin/Assets/1.Scripts/GoGame.cs
+++ Mannequin/Assets/1.Scripts/GoGame.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class GoGame : MonoBehaviour
 {
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void Change()
     {
         SceneManager.LoadScene("GameTempScene");
